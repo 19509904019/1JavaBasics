9c67923b974a2af0bd40b412c93dac910211c4d7
yujun <yujunaddress@163.com>
2023-06-18 16:41:11 +0800

    Change

---
+++ chapter05/src/com/field_method/filed/exer1/Employee.java
@@ -0,0 +1,20 @@
+package com.field_method.filed.exer1;
+
+/**
+ * ClassName:Employee
+ * Description:
+ * 声明员工类Employee:
+ *      属性：编号id、姓名name、年龄age、薪资salary
+ *
+ * 声明EmployeeTest测试类，并在main方法中，创建2个员工对象，并为属性赋值，并打印两个员工的信息
+ *
+ * @Author ZY
+ * @Create 2023/6/16 17:00
+ * @Version 1.0
+ */
+public class Employee {
+    int id;
+    String name;
+    int age;
+    double salary;
+}
---
+++ chapter05/src/com/field_method/filed/exer1/EmployeeTest.java
@@ -0,0 +1,17 @@
+package com.field_method.filed.exer1;
+
+/**
+ * ClassName:EmployeeTest
+ * Description:
+ *
+ * @Author ZY
+ * @Create 2023/6/16 17:05
+ * @Version 1.0
+ */
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.id = 15245;
+        System.out.println(employee.id);
+    }
+}
