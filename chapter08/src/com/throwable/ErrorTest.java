acb6800b9770ca1a42a789bf3f456ee24ea69118
yujun <yujunaddress@163.com>
2023-09-14 21:46:02 +0800

    Change

---
+++ chapter08/src/com/throwable/ErrorTest.java
@@ -0,0 +1,16 @@
+package com.throwable;
+
+/**
+ * ClassName:ErrorTest
+ * Description:
+ *
+ * @Author ZY
+ * @Create 2023/9/14 21:02
+ * @Version 1.0
+ */
+public class ErrorTest {
+    // 栈内存溢出的错误：StackOverflowError
+    public static void main(String[] args) {
+        main(args);
+    }
+}
