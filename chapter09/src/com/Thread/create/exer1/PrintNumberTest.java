31266eb6510b2912fee17ca85cfeb65b8125c9e0
yujun <yujunaddress@163.com>
2023-10-11 22:31:08 +0800

    Change

--- chapter09/src/com/Thread/create/exer1/PrintNumberTest.java
+++ chapter09/src/com/Thread/create/exer1/PrintNumberTest.java
@@ -8,7 +8,7 @@ package com.Thread.create.exer1;
  * @Create 2023/9/20 20:41
  * @Version 1.0
  */
-public class PrintNumberTest extends Thread {
+public class PrintNumberTest{
     public static void main(String[] args) {
         // 创建Thread类的匿名子类的匿名对象打印偶数
         new Thread() {
@@ -22,8 +22,9 @@ public class PrintNumberTest extends Thread {
             }
         }.start();
 
+
         // 创建Thread类的匿名子类的匿名对象打印奇数
-        new Thread() {
+        new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i <= 100; i++) {
@@ -32,7 +33,7 @@ public class PrintNumberTest extends Thread {
                     }
                 }
             }
-        }.start();
+        }).start();
     }
 }
 
---
+++ chapter17/src/LambdaTest1.java
@@ -0,0 +1,108 @@
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
+
+/**
+ * ClassName:LambdaTest1
+ * Description:
+ *
+ * @Author ZY
+ * @Create 2023/10/11 20:44
+ * @Version 1.0
+ */
+public class LambdaTest1 {
+    @Test
+    public void test1() {
+        // 语法格式一：无参，无返回值
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Runnable()...");
+            }
+        };
+        r1.run();
+
+        System.out.println("***********************");
+
+        Runnable r2 = () -> System.out.println("Runnable...");
+        r2.run();
+    }
+
+    // 语法格式2：Lambda需要一个参数，但是没有返回值
+    @Test
+    public void test2() {
+        Consumer<String> consumer = new Consumer<>() {
+            @Override
+            public void accept(String str) {
+                System.out.println(str);
+            }
+        };
+        consumer.accept("我是你，你是谁？");
+
+        System.out.println("*********************");
+
+        // 语法格式3：数据类型可以省略，因为可由编译器推断得出，称为“类型推断”
+        Consumer<String> con = (str) -> System.out.println(str);
+        con.accept("你管我是谁");
+    }
+
+    // 语法格式4：Lambda 若只要一个参数时，参数的小括号可以省略
+    public void test3() {
+        Consumer<String> consumer = new Consumer<>() {
+            @Override
+            public void accept(String str) {
+                System.out.println(str);
+            }
+        };
+        consumer.accept("我是你，你是谁？");
+
+        System.out.println("*********************");
+
+        // 小括号可以省略
+        Consumer<String> con = str -> System.out.println(str);
+        con.accept("你管我是谁");
+    }
+
+    // 语法格式5：Lambda 若有两个及以上的参数时，多条执行语句，并且可以有返回值
+    @Test
+    public void test5() {
+        Comparator<Integer> com1 = new Comparator<>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                System.out.println(o1);
+                System.out.println(o2);
+                return o1.compareTo(o2);
+            }
+        };
+        System.out.println(com1.compare(12,21));
+
+        System.out.println("***********************");
+
+        Comparator<Integer> com2 = (o1,o2) -> {
+            System.out.println(o1);
+            System.out.println(o2);
+            return o1.compareTo(o2);
+        };
+        System.out.println(com2.compare(12, 21));
+    }
+
+    // 语法格式6：当Lambda体只有一条语句时，return 与大括号若有则都可以省略
+    @Test
+    public void test6() {
+        Comparator<Integer> com1 = new Comparator<>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+        System.out.println(com1.compare(12,21));
+
+        System.out.println("***********************");
+
+        Comparator<Integer> com2 = (o1,o2) -> o1.compareTo(o2);
+        System.out.println(com2.compare(12, 21));
+    }
+
+
+}
